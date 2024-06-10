783d1086f95b53cfac7b51f42e950bdebb376d13
Felipe Fracasso <93887208+FelipeM-F@users.noreply.github.com>
2024-06-10 11:06:24 -0400

    menu header

--- Landing_Page/src/app.js
+++ Landing_Page/src/app.js
@@ -137,9 +137,3 @@ function closeModal() {
 
 }
 
-const modal = document.getElementById('modal');
-window.addEventListener('click', (e) => {
-    if (e.target !== modal) {
-        modal.style.display = 'none';
-    }
-});
