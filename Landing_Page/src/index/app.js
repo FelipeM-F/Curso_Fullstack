9477b6022dd5f0cf8d89defaa0c21f4ac9863a6a
Felipe Fracasso <93887208+FelipeM-F@users.noreply.github.com>
2024-06-23 17:42:52 -0400

    melhora da organização dos arquivos

--- Landing_Page/src/index/app.js
+++ Landing_Page/src/index/app.js
@@ -1,23 +1,5 @@
 
 
-document.getElementById('search-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    const searchQuery = document.getElementById('nome-jogo').value.toLowerCase();
-    const filteredGames = games.filter(game => game.title.toLowerCase().includes(searchQuery));
-    
-    if (filteredGames.length > 0) {
-        games.length = 0; // Clear the current games array
-        games.push(...filteredGames); // Add the filtered games to the array
-        currentPage = 1; // Reset to the first page
-        displayGames(currentPage);
-    } else {
-        const resultado = document.getElementById('resultado');
-        resultado.innerHTML = '<p>Nenhum jogo encontrado.</p>';
-        const pagination = document.getElementById('pagination');
-        pagination.innerHTML = '';
-    }
-});
-
 
 
 
--- Landing_Page/src/modal/modal.js
+++ Landing_Page/src/modal/modal.js
@@ -1,3 +1,39 @@
+async function loadModal(url, elementId) {
+    const response = await fetch(url);
+    const content = await response.text();
+    document.getElementById(elementId).innerHTML = content;
+    return content;
+}
+
+Promise.all([
+    loadModal('/src/modal/modal.html', 'modal-container'),
+
+]).catch(error => {
+    console.error('Error loading content:', error);
+});
+
+function register(event) {
+    event.preventDefault();
+    
+    const nickname = document.getElementById("nickname").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+  
+    console.log("Registro de usuário:", { nickname, email, password });
+    closeModal("modal-login");
+}
+
+function login(event) {
+    event.preventDefault();
+
+    const loginEmail = document.getElementById('loginEmail').value;
+    const loginPassword = document.getElementById('loginPassword').value;
+
+    console.log('Login do usuário:', { loginEmail, loginPassword });
+    closeModal('modal-login');
+}
+
+
 function openModal(modalId) {
 
     const modal = document.getElementById(modalId);
